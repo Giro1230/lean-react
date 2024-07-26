a635e7cda1509f1efae6e522cb39eed48c3251cd
giro <giro@nextpay.co.kr>
2024-07-26 10:37:29 +0900

    https://react.vlpt.us/basic/03-first-component.html
    참고 1장 5강 props 의 기본 사용법

--- src/App.js
+++ src/App.js
@@ -2,25 +2,8 @@ import React from 'react';
 import Hello from "./Hello";
 
 function App() {
-    const name = "react";
-    const style = {
-        backgroundColor: "black",
-        color: "aqua",
-        fontSize: "1.8rem",
-        padding: "1rem",
-    };
-
-
     return (
-        // Fragment : dom을 좀더 유동적으로 관히하기 위함
-        <>
-            {/* 주석은 화면에 보이지 않습니다 */}
-            /* 중괄호로 감싸지 않으면 화면에 보입니다 */
-            <Hello
-            />
-            <div style={style}>{name}</div>
-            <div className="gray-box"></div>
-        </>
+       <Hello name="react" />
     );
 }
 
